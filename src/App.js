54f1c9c612c31dd1ba86e99319da9cf8a410fd4b
Owen Lavall <owenlavall@Emmetts-MacBook-Air.local>
2023-05-06 10:54:29 +0000

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import About from "./component/About";
+import Portfolio from "./component/Portfolio";
+import Experience from "./component/Experience";
+import SocialLinks from "./component/SocialLinks";
+import Contact from "./component/Contact";
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Home></Home>
+      <About></About>
+      <Portfolio></Portfolio>
+      <Experience></Experience>
+      <SocialLinks></SocialLinks>
+      <Contact></Contact>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      black: '#000',
+      white: '#fff',
+    },
+    extend: {},
+    fontFamily: {
+      greatvibes: ["Great Vibes"],
+      kurale: ["Kurale"],
+    },
+  },
+  plugins: [],
+}
+
