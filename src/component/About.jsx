b30ac8de47da8e1126ba92d6eb47c993ea2b810d
Owen Lavall <owenlavall@Emmetts-MacBook-Air.local>
2023-05-06 19:27:31 +0000

    changes done

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -4,16 +4,16 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen  text-black md:mt-8"
+      className="w-full h-screen  text-black md:mt-1"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          <p className="text-4xl mx-auto font-bold inline border-b-4 border-gray-500 sm:text-center">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-20">
+        <p className="text-xl mt-8">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
           deserunt illum mollitia officiis qui exercitationem perferendis neque
           quasi a recusandae necessitatibus tempora iusto! Blanditiis error
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-blue mt-2 cursor-pointer"
+              className="group text-white w-fit px-6 py-3 transition ease-in-out delay-150 hover:bg-orange-500 my-2 flex items-center rounded-md bg-blue mt-2 cursor-pointer"
             >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
